7a57a9009960f0cccb83af74db1dc4dc076f2b40
Duquerson <sotodjgato@gmail.com>
2024-08-21 07:34:17 -0400

    changes confi vercel

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 import cors from "cors";
 import express from "express";
+import path from "path";
 import api from "./api/index.js";
 
 const app = express();
@@ -10,6 +11,10 @@ app.use(express.json());
 
 app.use("/api", api);
 const PORT = process.env.PORT || process.env.PORT_DEFAULT || 5000;
+app.use(express.static(path.join(__dirname, "./dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./dist/index.html"));
+});
 
 try {
 	app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
