2bc5ccd2673d7c378e0dfb0f468db1e1032e5ee4
Duquerson <sotodjgato@gmail.com>
2024-09-11 22:54:24 -0300

    update names shemas

--- api/schemas/todos.schema.js
+++ api/schemas/todos.schema.js
@@ -1,3 +1,7 @@
 import { z } from "zod";
-import { todoSchema } from "./todo.schema";
+const todoSchema = z.object({
+	id: z.string().uuid(),
+	description: z.string().min(1, { message: "La descripción no puede estar vacía" }).max(255, { message: "La descripción es demasiado larga" }),
+	completed: z.boolean(),
+});
 export const todosSchema = z.array(todoSchema);
