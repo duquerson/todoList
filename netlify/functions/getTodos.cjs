61120d8d21e590e546403e8d14d7a51bd44b54d6
Duquerson <sotodjgato@gmail.com>
2024-09-22 20:49:00 -0300

    test endpoint 2.5.1

--- netlify/functions/getTodos.cjs
+++ netlify/functions/getTodos.cjs
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 //import TodoService from "../../server/services/TodoService.js";
-const TodoService = require("../../server/services/TodoService.js");
+const TodoService = require("../../server/services/todos.service.js");
 export const handler = async () => {
 	const TODOS = await TodoService.getTodos();
 	return {
