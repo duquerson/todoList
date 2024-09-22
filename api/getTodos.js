3860489623feb0a43ae46c06f1d35d7e3bedf9f9
Duquerson <sotodjgato@gmail.com>
2024-09-22 19:10:03 -0300

    test endpoint 1.8

--- api/getTodos.js
+++ api/getTodos.js
@@ -1,6 +1,6 @@
 import TodoService from "../server/services/TodoService.js";
 export.handler = async ()=>{
-	const TODOS = TodoService.getTodos();
+	const TODOS = await TodoService.getTodos();
 	return {
 		statusCode: 200,
 		body: JSON.stringify(TODOS)
